429c3953b7456656e8ff054754e020d9f3569812
fateh bouretoua <bouretoua_fateh@hotmail.com>
2023-09-22 23:42:04 +0200

    Add StringWordAsParameter files

---
+++ StringWordAsParameter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static string GetUserInput(string word)
+    {
+        string message = $"Please enter a {word}:";
+        Console.WriteLine(message);
+        string text = Console.ReadLine();
+        return text;
+    }
+
+    static void Main()
+    {
+        // we can set word to "name," "value," or any other word, and the message will be adjusted accordingly.
+        Console.Write("Enter an argument word: ");
+        string chosenWord = Console.ReadLine();
+
+        string enteredText = GetUserInput(chosenWord);
+        Console.WriteLine($"You entered: {enteredText}");
+    }
+}
